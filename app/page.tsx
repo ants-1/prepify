65bbd6e52f67ce2385719797de29cf2455e8fae7
Anthony <anthonysalisbury98@outlook.com>
2026-01-09 21:45:26 +0000

    feat(auth): setup Google OAuth using Auth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import SignInForm from "@/components/SignInForm";
 import UsersList from "@/components/UsersList";
 
 export default async function Home() {
@@ -5,6 +6,7 @@ export default async function Home() {
     <div className="min-h-screen flex flex-col items-center justify-center">
       <h1 className="mb-8">Prepify</h1>
       <UsersList />
+      <SignInForm />
     </div>
   );
 }
---
+++ components/SignInForm.tsx
@@ -0,0 +1,16 @@
+import { signIn } from "@/auth";
+
+export default function SignInForm() {
+  return (
+    <>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+      >
+        <button type="submit">Sign-in with Google</button>
+      </form>
+    </>
+  )
+}
